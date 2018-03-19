3d8661a76cd4b9189ae4e0cd219e04aaeda32c1f
Eran Messeri <eranm@google.com>
2018-03-19 16:48:47 +0000

    KeyChain: Upgrade keys database
    A new table was added to KeyChain's grants.db, storing the
    user-visibilty state of each alias KeyChain manages.
    
    Formerly it was not created during an upgrade, which led to existing
    keys being classified as non-user-selectable and essentially not usable.
    
    Now, during upgrade, the table for user-visibility is created and the
    existing aliases are all set as user-selectable in it.
    
    Test: m -j RunKeyChainRoboTests
    Bug: 73898958
    Change-Id: I3b92a957f4e949c13363769ece531af438895ff9

--- robotests/src/com/android/keychain/internal/GrantsDatabaseTest.java
+++ robotests/src/com/android/keychain/internal/GrantsDatabaseTest.java
@@ -19,7 +19,11 @@ package com.android.keychain.internal;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import com.android.keychain.TestConfig;
 import org.junit.Assert;
 import org.junit.Before;
@@ -37,6 +41,12 @@ public final class GrantsDatabaseTest {
     private static final String DUMMY_ALIAS2 = "another_dummy_alias";
     private static final int DUMMY_UID = 1000;
     private static final int DUMMY_UID2 = 1001;
+    // Constants duplicated from GrantsDatabase to make sure the upgrade tests catch if the
+    // name of one of the fields in the DB changes.
+    static final String DATABASE_NAME = "grants.db";
+    static final String TABLE_GRANTS = "grants";
+    static final String GRANTS_ALIAS = "alias";
+    static final String GRANTS_GRANTEE_UID = "uid";
 
     private GrantsDatabase mGrantsDB;
 
@@ -107,7 +117,7 @@ public final class GrantsDatabaseTest {
     public void testPurgeOldGrantsDoesNotDeleteGrantsForExistingPackages() {
         mGrantsDB.setGrant(DUMMY_UID, DUMMY_ALIAS, true);
         PackageManager pm = mock(PackageManager.class);
-        when(pm.getPackagesForUid(DUMMY_UID)).thenReturn(new String[]{"p"});
+        when(pm.getPackagesForUid(DUMMY_UID)).thenReturn(new String[] {"p"});
         mGrantsDB.purgeOldGrants(pm);
         Assert.assertTrue(mGrantsDB.hasGrant(DUMMY_UID, DUMMY_ALIAS));
     }
@@ -145,5 +155,58 @@ public final class GrantsDatabaseTest {
         Assert.assertFalse(mGrantsDB.isUserSelectable(DUMMY_ALIAS));
         mGrantsDB.setIsUserSelectable(DUMMY_ALIAS, true);
         Assert.assertTrue(mGrantsDB.isUserSelectable(DUMMY_ALIAS));
+    }
+
+    private class V1DatabaseHelper extends SQLiteOpenHelper {
+        public V1DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null /* CursorFactory */, 1);
+        }
+
+        @Override
+        public void onCreate(final SQLiteDatabase db) {
+            db.execSQL(
+                    "CREATE TABLE "
+                            + TABLE_GRANTS
+                            + " (  "
+                            + GRANTS_ALIAS
+                            + " STRING NOT NULL,  "
+                            + GRANTS_GRANTEE_UID
+                            + " INTEGER NOT NULL,  "
+                            + "UNIQUE ("
+                            + GRANTS_ALIAS
+                            + ","
+                            + GRANTS_GRANTEE_UID
+                            + "))");
+        }
+
+        @Override
+        public void onUpgrade(final SQLiteDatabase db, int oldVersion, final int newVersion) {
+            throw new IllegalStateException("Existing DB must be dropped first.");
+        }
+    }
+
+    @Test
+    public void testUpgradeDatabase() {
+        // Close old DB
+        mGrantsDB.destroy();
+        // Create a new, V1 database.
+        Context context = RuntimeEnvironment.application;
+        context.deleteDatabase(DATABASE_NAME);
+        V1DatabaseHelper v1DBHelper = new V1DatabaseHelper(context);
+        // Fill it up with a few records
+        final SQLiteDatabase db = v1DBHelper.getWritableDatabase();
+        String[] aliases = {"alias-1", "alias-2", "alias-3"};
+        for (String alias : aliases) {
+            final ContentValues values = new ContentValues();
+            values.put(GRANTS_ALIAS, alias);
+            values.put(GRANTS_GRANTEE_UID, 123456);
+            db.insert(TABLE_GRANTS, GRANTS_ALIAS, values);
+        }
+
+        // Test that the aliases were made user-selectable during the upgrade.
+        mGrantsDB = new GrantsDatabase(RuntimeEnvironment.application);
+        for (String alias : aliases) {
+            Assert.assertTrue(mGrantsDB.isUserSelectable(alias));
+        }
     }
 }
